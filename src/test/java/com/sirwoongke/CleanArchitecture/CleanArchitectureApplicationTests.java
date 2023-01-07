d7a4cb646bab5dfc420c4f0a489df49b1c6d7ecb
sirwoongke <fzwoongbin@naver.com>
2023-01-07 19:49:13 +0900

    init

---
+++ src/test/java/com/sirwoongke/CleanArchitecture/CleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirwoongke.CleanArchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
